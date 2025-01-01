2f3adcc611c7f4578af06e7782137f1096bf2027
Gelzieny R. Martins <gelzieny@gmail.com>
2025-01-01 11:34:35 -0300

    Carregando variáveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { app } from './app'
+import { env } from './env'
+
+const port = env.PORT
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: port,
   })
   .then(() => {
-    console.log('🚀 HTTP Server is running on http://localhost:3333')
+    console.log(`🚀 HTTP Server is running on http://localhost:${port}`)
   })

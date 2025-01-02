4facf06d5eb0334559278b89e215dd502341fcda
Gelzieny R. Martins <gelzieny@gmail.com>
2025-01-02 14:03:41 -0300

    Inversão de dependências

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,19 @@
 import { defineConfig } from 'eslint-define-config'
 
 export default defineConfig({
-  ignores: ['node_modules', 'dist', 'build'], // Substitui o antigo .eslintignore
+  ignores: ['node_modules', 'dist', 'build'],
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: [
-    '@rocketseat/eslint-config', // Certifique-se de que o pacote está instalado
-  ],
+  extends: ['@rocketseat/eslint-config'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   rules: {
-    // Adicione ou sobrescreva regras aqui, se necessário
+    camelcase: 'off',
+    'no-useless-constructor': 'off',
   },
 })
